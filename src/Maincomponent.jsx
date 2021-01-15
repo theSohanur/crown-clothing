cedb08bb81dda6b6a5516b39b00c9706e70bcfb2
Sohanur Rahman <sohanur064@gmail.com>
2021-01-15 18:32:17 +0600

    Create Hompage Component

---
+++ src/Maincomponent.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Homepage from './components/Homepage.component';
+
+const Maincomponent = () => {
+    return (
+        <div>
+            <Homepage/>
+        </div>
+    )
+}
+
+export default Maincomponent
